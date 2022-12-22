19c58650cfb88bf1d3876d72366bffa93277f8d8
Patrick Dunn <pmdunn78@gmail.com>
2022-12-22 09:45:29 -0500

    adding pseudocode for full functionality

--- script.js
+++ script.js
@@ -1,11 +1,4 @@
-var thisIsTrue = true;
 
-var tile1 = document.querySelector('#one');
-// var tile2 = document.querySelector('#1-2');
-// var tile3 = document.querySelector('#1-3');
-// var tile4 = document.querySelector('#1-4');
-// var tile5 = document.querySelector('#1-5');
-// var tile6 = document.querySelector('#1-6');
 
 var activeRows = [
   {
@@ -43,6 +36,59 @@ const setActiveRow = (indexToBeActive, arr) => {
 }
 
 var currentActiveCell = 1;
+var initialActiveCell = 1;
+
+// Todos:
+  // set conditional where if currentActiveCell === initialActiveCell + 5, 
+  // THEN set currentActiveCell to be initialActiveCell + 4
+
+  // set the first row as active row
+  // add currentActiveRow variable set as an integer
+  
+  // add conditional to only run the 'keydown' event listener for the current active row
+    // first check to see what the currentActiveRow is
+    // then set the currentActiveCell and initialActiveCell values accordingly 
+  
+  // add "Generate guesses" button
+    // when "Generate guesses" is fired --> 
+      // set the currentActiveRow
+      // set the necessary variables
+      // run all the calculations
+      // append an element to the page for each word in the results array
+      // include the recommended word up top
+
+  // add CSS classes for:
+    // correct position + correct letter
+    // incorrect position + correct letter
+    // incorrect position + incorrect letter (lettersThatCantBeUsed)
+  
+  // include 'data-absolute', 'data-necessary' and 'data-verboten' values
+  // add event listener for the 'tile' class =>
+    // if !data-absolute && !data-necessary && !data-verboten => 
+      // data-absolute=true
+      // add styling class for correct position + correct letter
+      // else if !data-necessary && !data-verboten =>
+        // data-necessary=true
+        // add styling class for incorrect position + correct letter
+      // else =>
+        // data-verboten=true
+        // add styling class for incorrect position + incorrect letter (lettersThatCantBeUsed)
+
+  // create variables for 
+    // var firstMustBe = '';
+    // var secondMustBe = '';
+    // var thirdMustBe = '';
+    // var fourthMustBe = '';
+    // var fifthMustBe = '';
+
+    // var lettersThatCantBeUsed = '';
+    // var mustBeUsed = '';
+    
+    // var positionOneCantBe = '';
+    // var positionTwoCantBe = '';
+    // var positionThreeCantBe = '';
+    // var positionFourCantBe = '';
+    // var positionFiveCantBe = '';
 
 document.addEventListener('keydown', function(e) {
   
